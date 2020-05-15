17440df6953583e011d1c23696858c66c19f30f3
Marcus Ackland <mackland@kth.se>
2020-05-15 21:57:24 +0100

    match incoming orders with ones resting on the book and modify or remove those if traded

--- orderbook.py
+++ orderbook.py
@@ -1,8 +1,16 @@
+#TODO:
+# add_order_to_book
+# add_trade_to_book
+# confirm_trade
+
 class Orderbook():
     def __init__(self):
         self.order_history = []
         self.bid_book = {}
+        self._bid_book_prices = []
         self.ask_book = {}
+        self._ask_book_prices = []
+        self.traded = False
 
     def add_order_to_history(self, order):
 
@@ -21,5 +29,72 @@ class Orderbook():
     def process_order(self, order): #this will do most of the work
 
     def match_trade(self, order):
+        ''' 
+        Match order with resting orders on the book, once matched will remove resting order or modify by number of shares of the incoming order
+        '''
+        self.traded = True
+         
+        if order['side'] == 'buy':
+            book_prices = self._ask_book_prices
+            book = self._ask_book
+            remainder = order['quantity']
 
+            while remainder > 0:
+                if book_prices:
+                    price = book_prices[0]
+                    if order['price'] >= price: 
+                        book_order_id = book[price]['order_ids'][0]
+                        book_order = book[price]['orders'][book_order_id]
+                        if remainder >= book_order['quantity']: #if buy order larger than order on the book, execute at that price and remove the resting order
+                            self.confirm_trade(order['timestamp'], book_order['side'], book_order['quantity'], 
+                                    book_order['order_id'], book_order['price'])
+                            self.add_trade_to_book(book_order['order_id'], book_order['timestamp'], order['order_id'], 
+                                    order['timestamp'], book_order['price'], book_order['quantity'], order['side'])
+                            self.remove_order(book_order['side'], book_order['price'], book_order['order_id'])
+                            remainder -= book_order['quantity']
+                        else: # trade will take place but will only reduce the resting order quantity by # of shares
+                            self.confirm_trade(order['timestamp'], book_order['side'], remainder, 
+                                    book_order['order_id'], book_order['price'])
+                            self.add_trade_to_book(book_order['order_id'], book_order['timestamp'], order['order_id'], 
+                                    order['timestamp'], book_order['price'], remainder, order['side'])
+                            self.modify_order(book_order['side'], remainder, book_order['order_id'], book_order['price'])
+                            break
+                    else: #no immediate match, add order to book
+                        order['quantity'] = remainder
+                        self.add_order_to_book(order)
+                        break
+                else:
+                    print('No orders on book after order {0}'.format(order))
+                    break
+        else: #order['side'] == 'sell'
+            book_prices = self._bid_book_prices
+            book = self._bid_book
+            remainder = order['quantity']
+            while remainder > 0:
+                if book_prices:
+                    price = book_prices[-1]
+                    if order['price'] <= price:
+                        book_order_id = book[price]['order_ids'][0]
+                        book_order = book[price]['orders'][book_order_id]
+                        if remainder >= book_order['quantity']:
+                            self.confirm_trade(order['timestamp'], book_order['side'], book_order['quantity'], 
+                                    book_order['order_id'], book_order['price'])
+                            self.add_trade_to_book(book_order['order_id'], book_order['timestamp'], order['order_id'], 
+                                    order['timestamp'], book_order['price'], book_order['quantity'], order['side'])
+                            self.remove_order(book_order['side'], book_order['price'], book_order['order_id'])
+                            remainder -= book_order['quantity']
+                        else: # trade will take place but will only reduce the resting order quantity by # of shares
+                            self.confirm_trade(order['timestamp'], book_order['side'], remainder, book_order['order_id'], book_order['price'])
+                            self.add_trade_to_book(book_order['order_id'], book_order['timestamp'], order['order_id'], 
+                                    order['timestamp'], book_order['price'], remainder, order['side'])
+                            self.modify_order(book_order['side'], remainder, book_order['order_id'], book_order['price'])
+                            break
+ 
+                    else:
+                        order['quantity'] = remainder
+                        self.add_order_to_book(order)
+                        break
+                else:
+                    print('No orders on bid book with order {0}'.format(order))
+                    break
 
